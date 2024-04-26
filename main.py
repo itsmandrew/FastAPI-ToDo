d0e2e2314eb7ea9bf7be78c4e5a5e76ea4aaa64b
Andrew Chang <89715746+itsmandrew@users.noreply.github.com>
2024-04-26 01:40:17 -0700

    finished basic todo api

--- main.py
+++ main.py
@@ -17,11 +17,36 @@ async def get_todos():
     return {"todos" : todos}
 
 # Get single todo
+@app.get("/todos/{todo_id}")
+async def get_todo(todo_id: int):
+    for todo in todos:
+        if todo.id == todo_id:
+            return {"todo": todo}
+    return {"message" : "No todos found"}
 
 
 # Create a todo
+@app.post("/todos")
+async def create_todos(todo: Todo):
+    todos.append(todo)
+    return {"todos" : "Todo has been added"}
 
 # updata a todo
+@app.put("/todos/{todo_id}")
+async def update_todo(todo_id: int, todo_obj: Todo):
+    for todo in todos:
+        if todo.id == todo_id:
+            todo.id = todo_id
+            todo.item = todo_obj.item
+            return {"todos": todo}
+    return {"message" : "No todos found to update"}
 
 
 #delete a todo
+@app.delete("/todos/{todo_id}")
+async def delete_todo(todo_id: int):
+    for todo in todos:
+        if todo.id == todo_id:
+            todos.remove(todo)
+            return {"message": "todo has been deleted"}
+    return {"message" : "No todos found"}
--- models.py
+++ models.py
@@ -1,2 +1,7 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
+
+
+class Todo(BaseModel):
+    id: int
+    item: str
